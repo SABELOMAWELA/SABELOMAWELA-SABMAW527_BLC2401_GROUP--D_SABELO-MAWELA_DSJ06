96f0ee0e787f03e674e7c985e6b617da1cf78415
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-05-24 11:28:55 +0200

    completed Foreach basics

---
+++ StudentNo_Classcode_Group_Name-Surname_DJS06-main/scripts.js
@@ -0,0 +1,33 @@
+// A list of provinces:
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State']
+
+// A list of names:
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
+
+// A list of products with prices:
+const products = [
+  { product: 'banana', price: "2" },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: "8" },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+names.forEach(name => console.log(name))
+
+provinces.forEach(province => console.log(province))
+
+names.forEach((name, index) => console.log(name, provinces[index]));
+
+
+
+
+
+
+
+
+
+ names||provinces.forEach(function(mixed){
+   console.log(mixed)
+  })
